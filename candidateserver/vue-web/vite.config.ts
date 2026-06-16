faa71445809a856878f509855066ca135cb3759e
LMF709268224 <709268224@qq.com>
2026-06-16 13:24:34 +0800

    docker

--- candidateserver/vue-web/vite.config.ts
+++ candidateserver/vue-web/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "@embedpdf/snippet": fileURLToPath(new URL("./node_modules/@embedpdf/snippet/dist/embedpdf.js", import.meta.url)),
+      "@vue/reactivity": fileURLToPath(new URL("./node_modules/@vue/reactivity/dist/reactivity.esm-bundler.js", import.meta.url)),
+      "@vue/runtime-core": fileURLToPath(new URL("./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js", import.meta.url)),
+      "@vue/runtime-dom": fileURLToPath(new URL("./node_modules/@vue/runtime-dom/dist/runtime-dom.esm-bundler.js", import.meta.url)),
+      "@vue/shared": fileURLToPath(new URL("./node_modules/@vue/shared/dist/shared.esm-bundler.js", import.meta.url)),
       "@vue/devtools-api": fileURLToPath(new URL("./node_modules/@vue/devtools-api/dist/index.js", import.meta.url)),
     },
   },
